bafb31101117806d1b86095e986cca01f532007c
Dmitry Fedotov <fedotov-ds1989@yandex.ru>
2022-08-29 17:01:14 +0300

    task_013

---
+++ task_013/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+if(Math.Abs(number) < 100)
+{
+    Console.WriteLine(number + " -> третьей цифры нет");
+}
+else
+{
+string num = Math.Abs(number).ToString();
+int[] array = new int[num.Length];
+int length = num.Length;
+int index = 0;
+while(index < length)
+{
+    array[index]=int.Parse(num[index].ToString());
+    index++;
+}
+Console.WriteLine(number + " -> " + array[2]);
+}
