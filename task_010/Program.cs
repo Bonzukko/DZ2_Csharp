2aff3b6e7d45045ee7221f41dc964221b549c30b
Dmitry Fedotov <fedotov-ds1989@yandex.ru>
2022-08-29 16:53:49 +0300

    task_010

---
+++ task_010/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+while(Math.Abs(num)<100 || Math.Abs(num)>999)
+{
+    Console.WriteLine("Введите трёхзначное число");
+    num = Convert.ToInt32(Console.ReadLine());
+ }
+Console.Clear();
+Console.WriteLine(num + " -> " + Math.Abs(num) % 100/10);
